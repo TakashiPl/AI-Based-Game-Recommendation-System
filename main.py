ae8fd73fe83841595290e671e9d8b95aa36966c3
Mateusz Gadzina <gadzinam28@gmail.com>
2026-04-05 22:48:41 +0200

    add project structure

---
+++ main.py
@@ -0,0 +1,19 @@
+from utils import get_non_empty_string, get_number
+from logic import get_recommendations
+
+def main():
+    age = get_number("Enter your age: ", int, 0, 120)
+    interest = get_non_empty_string(
+        "Enter your interests (comma separated, e.g. games, music): "
+        )
+
+    recommendations = get_recommendations(age, interest)
+
+    print("\n--- RESULT ---")
+    if recommendations:
+        print("We recommend:", ", ".join(recommendations))
+    else:
+        print("No recommendations found. Try: games, music")
+
+if __name__ == "__main__":
+    main()
