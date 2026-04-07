acf2df0ac486f6018437952b655b73d164cffb0d
Mateusz Gadzina <gadzinam28@gmail.com>
2026-04-07 16:36:23 +0200

    refactor: added comments to code and improved logic code

--- logic.py
+++ logic.py
@@ -1,6 +1,8 @@
 from data import DATA
 import heapq
 
+
+#Function to classify users age into one of available age groups
 def classify_age(age):
     if age < 13:
         return "kid"
@@ -8,11 +10,13 @@ def classify_age(age):
         return "teen"
     return "adult"
 
+#Function to get recommendations using point-based system
 def get_recommendations(age, interest, debug=False):
     group = classify_age(age)
     interests = [i.lower().strip() for i in interest.split(",")]
     recommendations = []
 
+    #For debug, to see if it works
     if debug:
         print("DEBUG age group:", group)
         print("DEBUG interests:", interests)
@@ -20,14 +24,12 @@ def get_recommendations(age, interest, debug=False):
 
     for d in DATA:
         obiekt = {"name": d["name"], "points" : 0}
-        if debug:
-            print(recommendations)
-            print(d["category"])
         if d["category"] in interests:
                     obiekt["points"] += 3
         if d["age_group"] == group:
                     obiekt["points"] += 2
-        recommendations.append(obiekt)
+        if obiekt["points"] > 0:
+            recommendations.append(obiekt)
 
-    
+    #Returns three recommendations with most points
     return heapq.nlargest(3, recommendations, key=lambda x: x['points'])
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+#Function to check whether the number provided for us age is a number and is a certain type (default = int)
 def get_number(prompt, cast_type=int, min_val=None, max_val=None):
     while True:
         try:
@@ -13,6 +14,7 @@ def get_number(prompt, cast_type=int, min_val=None, max_val=None):
         except ValueError:
             print("Provided value is not a number")
 
+#Function to check whether the string is not empty
 def get_non_empty_string(prompt):
     while True:
         value = input(prompt)
